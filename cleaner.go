79832ac1dfea5a9cad43764c1a2f330d9ea0c47d
Ahmy Yulrizka <yulrizka@gmail.com>
2016-12-03 23:29:50 +0100

    cleanup file < 60 days

--- cleaner.go
+++ cleaner.go
@@ -11,13 +11,16 @@ import (
 )
 
 var (
-	dryRun   = true
+	delete   = false
 	keepDays = 60
+
+	deleted = 0
 )
 
 func main() {
 	searchDir := flag.String("path", "scores", "path contains json file to clean up")
 	flag.IntVar(&keepDays, "days", 60, "days to keep the file")
+	flag.BoolVar(&delete, "f", false, "actually delete the file")
 	flag.Parse()
 
 	log.Printf("processing %q\n", *searchDir)
@@ -33,6 +36,8 @@ func main() {
 	if err != nil {
 		log.Fatal("failed processing directory", err)
 	}
+
+	log.Printf("deleted %d files", deleted)
 
 }
 
@@ -55,11 +60,12 @@ func processFile(path string) {
 
 	if age > time.Duration(keepDays)*24*time.Hour {
 		log.Printf("deleting %s (age %f)", path, age.Hours()/24)
-		if !dryRun {
+		if delete {
 			if err := os.Remove(path); err != nil {
 				log.Printf("failed removing %q, %s", path, err)
 			}
 		}
+		deleted++
 	} else {
 		log.Printf("skiping %s (age %f)", path, age.Hours()/24)
 	}
